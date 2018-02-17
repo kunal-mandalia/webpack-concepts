4c9a5ee4d4b781a259f4e18895dce51069a448b4
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-02-17 21:16:07 +0000

    feat: Setup global lodash depedency example

---
+++ src/index.js
@@ -0,0 +1,9 @@
+function component() {
+	var element = document.createElement('div')
+	// Lodash is required to run the line below
+	element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+	return element
+}
+
+document.body.appendChild(component())
+
